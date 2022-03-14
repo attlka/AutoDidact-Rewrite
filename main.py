e55f7d23cc5ccd61451e67af8590720c5d19fbd9
Daniel James <86825914+attlka@users.noreply.github.com>
2022-03-14 08:08:43 +0000

    unfucked async routines

--- main.py
+++ main.py
@@ -6,22 +6,10 @@ import openai
 my_secret = os.environ['gptkey']
 openai.api_key = ('gptkey')
 
+#Create a Discord bot that answers questions with GPT-3
 client = discord.Client()
 
-#Summons bot to server
-@client.event
-async def on_ready():
-	print('Logged in as')
-	print(client.user.name)
-	print(client.user.id)
-	print('------')
-
-@client.event
-async def on_message(message):
-	if message.content.startswith('!autodidact'):
-		await message.channel.send('Autodidact on standby!')
-
-#lets user ask question to gpt-3
+@asyncio.coroutine
 @client.event
 async def on_message(message):
 	if message.content.startswith('!ask'):
@@ -30,8 +18,10 @@ async def on_message(message):
 		response = openai.Completion.create(engine='text-davinci-001', prompt="You are a knowledgable speaker answering questions about Philosophy as a subject. You can only use information that can be found on the following website, The Wikipedia Page for Philosophy, to inform your answers: https://en.wikipedia.org/wiki/Philosophy. Please do not answer with information from other sources. + question\n", temperature=0.07, max_tokens=300)
 		await message.channel.send(response.choices[0]['text'])
 
+@asyncio.coroutine
 @client.event
 async def on_message(message):
 	if message.content.startswith('!help'):
 		await message.channel.send('!ask <question> - Ask me a question!')
 		await message.channel.send('!help - Displays all commands!')
+
